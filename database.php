c93982c3a7a9da85c4891ad449367f04b69a9723
Dinhduy01 <90381153+Dinhduy01@users.noreply.github.com>
2021-10-27 00:11:34 +0700

    Add files via upload

---
+++ database.php
@@ -0,0 +1,6 @@
+<?php
+$con= mysqli_connect ("localhost","root",'','bt1');
+if(!$con)
+{ 
+ die('could not connect:' );
+}
---
+++ index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<title>AUCTIONING FORUM</title>
+
+<meta name="keywords" content="photo blog template, free website layout, CSS, download HTML" />
+
+<meta name="description" content="Photo Blog Template, Free xHTML/CSS Layout, Free Website Template from TemplateMo.com" />
+
+<link href="templatemo_style.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+<div id="templatemo_container">
+
+  <div id="templatemo_left_section">
+
+   <div id="templatemo_left_header">
+
+     <div id="templatemo_left_title"> AUCTIONING FORUM </div>
+
+     <div id="templatemo_left_slogan"> Enjoy With Bidding Application...</div>
+
+     <div id="templatemo_latest"><a href="#"> NEW PRODUCTS </a></div>
+
+   </div>
+
+   <div id="templatemo_left_menu">
+
+     <div class="templatemo_topmenu">
+
+       <ul>
+         <li><a href="index.php" class="current">HOME</a></li>
+         <li><a href="user.php">FREE SIGN UP</a> </li>
+         <li><a href="product.php">PRODUCTS</a></li>
+         <li><a href="contact.php">CONTACT US</a></li>
+          <li><a href="admin.php">ADMIN LOGIN</a></li>
+       </ul>
+
+      </div>
+
+   </div>
+
+   <div id="templatemo_left_content">
+
+     <h1>WELCOME TO AUCTIONING FORUM</h1>
+      
+     <p>Auctioning Forum is web application developed using PHP as server scripting language,HTML,CSS as front end tools and mysql for database.
+		Auctioning Forum is web based auctioning site,in which the sellers who want to auction their art works can register themselves and become a auctioneer,then he can post his art work for auction biding,the viewers who are interested in buying the product can also become a member buyer,who bid for auctions.
+ </p>
+
+     <p>Mauris quis nulla sed ipsum pretium sagittis. Suspendisse feugiat. Ut sodales libero ut odio. Maecenas venenatis metus eu est. In sed risus ac felis varius bibendum. Nulla imperdiet congue metus. </p>
+
+     <h3>BEST PHOTOS OF THIS WEEK</h3>
+
+     <p><img src="images/templatemo_photo1.jpg" alt="templatemo.com" width="427" height="113" /></p>
+
+     <h3>LATEST POSTS</h3>
+
+     <div class="templatemo_content_left"><strong>POST DATE: 10-10-2021<br />
+     </strong>
+
+      Nulla enim nibh, consectetuer sed, vestibulum elementum, sagittis nec, diam. Mauris blandit vehicula neque.<br />
+      <br />
+      Donec venenatis. Cras urna metus, feugiat non, consectetuer quis, pretium quis, nunc. Nullam pede purus, tempor a, imperdiet in, porttitor at, nulla. </div>
+
+     <div class="templatemo_content_right"><strong>POST DATE: 05-10-2021<br />
+     </strong>
+
+      Aenean cursus. Maecenas aliquam, ligula id egestas suscipit, nisi sapien dignissim nibh, ac vestibulum lorem urna in neque. <br />
+      <br />
+      Aenean eget tortor eget ipsum aliquet porta. Vestibulum quis pede non urna venenatis vehicula. Praesent vel diam. Cras sed leo tempor neque. </div>
+
+   </div>
+
+  </div>
+
+  <div id="templatemo_right_section">
+
+    <div id="templatemo_right_top"></div>
+
+    <div id="templatemo_right_search">
+
+      <form id="form1" name="form1" method="post" action="">
+      </form>
+
+    </div>
+
+    <div id="templatemo_memberlog">
+
+      <h2>MEMBER LOGIN</h2>
+      <form id="form2" name="form2" method="post" action="user_login_code.php">
+        <table width="100%" border="0">
+          <tr>
+            <td width="24%">E-Mail </td>
+            <td width="7%">:</td>
+            <td width="69%"><label for="name"></label>
+            <input type="text" name="name" id="name"  /></td>
+          </tr>
+          <tr>
+            <td>Password</td>
+            <td>:</td>
+            <td><input type="password" name="pass" id="pass"  /></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td><input type="submit" name="button3" id="button3" value="Login" /></td>
+          </tr>
+        </table>
+        <p><strong>New Member?</strong> <a href="user.php"><strong>Register Now!</strong></a> </p>
+      </form>
+    </div>
+
+    <div id="templatemo_category">
+      <h2>CATEGORIES</h2>
+
+      <div class="templatemo_rightmenu">
+
+        <ul>
+
+           <li><a href="#">Latest Collections</a></li>
+
+          <li> <a href="#">2013 Collections</a></li>
+
+          <li> <a href="#">Vechile</a></li>
+
+          <li> <a href="#">Lands</a></li>
+
+          <li> <a href="#">Modren Arts</a></li>
+
+          <li> <a href="#">Books</a></li>
+
+        </ul>
+
+      </div>
+
+    </div>
+
+    <div id="templatemo_contact"><h2>QUICK CONTACT</h2>
+
+      <p>Tel: 010-100-1001<br />
+
+        Fax: 020-200-2002<br />
+
+        Email: info [at] AUCTIONING FORUM</p>
+
+      </div>
+
+  </div>
+
+  <div id="templatemo_footer">Copyright ©  Your AUCTIONING FORUM | Designed by <a href="" target="_parent" title="">Live Technologies</a></div>
+
+</div>
+</body>
+</html>
