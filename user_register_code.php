4f4340c415351ef1cf913816b49d7a31d12457a2
Dinhduy01 <90381153+Dinhduy01@users.noreply.github.com>
2021-10-27 09:54:04 +0700

    Add files via upload

---
+++ user_register_code.php
@@ -0,0 +1,28 @@
+<?php 
+ob_start();
+include 'database.php';
+
+$first_name=$_POST['f_name'];
+$last_name=$_POST['l_name'];
+$email=$_POST['mail'];
+$pass=$_POST['pwd'];
+$dob=$_POST['dob'];
+$gender=$_POST['gen'];
+$address=$_POST['add'];
+$phne=$_POST['ph'];
+
+$pass_hash= password_hash($pass,PASSWORD_DEFAULT);
+$i="insert into user_registration values('','".$first_name."','".$last_name."','".$email."','".$pass_hash."','".$dob."','".$gender."','".$address."','".$phne."')";
+
+$vv=mysqli_query($con,$i) or die("error".mysqli_error($con));
+
+if($vv)
+{
+//echo ("registed");
+header("location:user.php");
+}
+else
+{
+echo ("Not registed");
+}
+?>
