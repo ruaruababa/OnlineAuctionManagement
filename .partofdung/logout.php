fda673bfd502d6cc846032e0c3607fad2d275828
DungZizu <90980099+DungZizu@users.noreply.github.com>
2021-11-01 16:00:37 +0700

    Update logout.php

--- .partofdung/logout.php
+++ .partofdung/logout.php
@@ -1,14 +1,7 @@
 <?php
-// Initialize the session
-session_start();
- 
-// Unset all of the session variables
-$_SESSION = array();
- 
-// Destroy the session.
-session_destroy();
- 
-// Redirect to login page
-header("location: login.php");
-exit;
+    session_start(); 
+    if(isset($_SESSION['loginOK'])){
+        unset($_SESSION['loginOK']);
+        header("Location:index.php");
+    }
 ?>
