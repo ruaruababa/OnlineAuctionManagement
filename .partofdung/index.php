665735db62d57334d1cb305f3d373ae2ef135c69
DungZizu <90980099+DungZizu@users.noreply.github.com>
2021-11-01 19:38:57 +0700

    Update and rename welcome.php to index.php

---
+++ .partofdung/index.php
@@ -0,0 +1,83 @@
+<?php
+include('navbar.php');
+?>
+
+<section class="vh-100">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6 text-black">
+
+                <div class="px-5 ms-xl-4">
+                    <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
+                    <span class="h1 fw-bold mb-0"> <img src="images/eth.png" style="width: 100px;" alt=""> Welcome
+                        !</span>
+                </div>
+
+                <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
+
+                    <form style="width: 23rem;" id="form2" name="form2" method="post" action="user_login_code.php">
+
+                        <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Log in</h3>
+
+                        <div class="form-outline mb-4">
+                            <input type="text" name="name" id="name" class="form-control form-control-lg" />
+                            <label class="form-label" for="form2Example18">Email address</label>
+                        </div>
+
+                        <div class="form-outline mb-4">
+                            <input type="password" name="pass" id="pass" class="form-control form-control-lg" />
+                            <label class="form-label" for="form2Example28">Password</label>
+                        </div>
+                        <div id="response">
+                               
+                        </div>
+
+                        <div class="pt-1 mb-4">
+                            <button class="btn btn-info btn-lg btn-block" type="submit" name="button" id="button">Login</button>
+                        </div>
+                        <p>Don't have an account? <a href="Registration-form.php" class="link-info">Register here</a></p>
+
+                    </form>
+
+                </div>
+
+            </div>
+            <div class="col-sm-6 px-0 d-none d-sm-block" style="margin-top: 70px;">
+                <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="images/nft8.jpeg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="images/nft4.jpeg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="images/nft7.jpeg" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+include('footer.php');
+?>
+
+</body>
+
+</html>
