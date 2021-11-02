96f02ad738259863cc08ea3193bb5d92b43a78d1
DungZizu <90980099+DungZizu@users.noreply.github.com>
2021-11-02 23:10:06 +0700

    Update admin_login.php

--- .partofdung/admin_login.php
+++ .partofdung/admin_login.php
@@ -71,12 +71,10 @@ include('navbar_admin.php');
     </div>
 </section>
 
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+
 <?php
-include('footer.php');
+include('../footer.php');
 ?>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-</script>
-</body>
-
-</html>
