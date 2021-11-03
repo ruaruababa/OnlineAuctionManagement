73311a0c2cd9d7a2ac2d50c7fad8f4d85a9b92db
Yang <54880067+ruaruababa@users.noreply.github.com>
2021-11-04 00:58:37 +0700

    Fixed error, convert price string to int

--- process_edit_product.php
+++ process_edit_product.php
@@ -3,6 +3,7 @@ if (isset($_POST['button'])) {
     $p_id = $_POST['p_id'];
     $category = $_POST['category'];
     $price = $_POST['price'];
+    $price1 = (int)$price;
     $p_img = $_POST['image'];
     $p_desp = $_POST['desp'];
 
@@ -10,7 +11,7 @@ if (isset($_POST['button'])) {
     $p_name = $_POST['p_name'];
     include 'database.php';
 
-    $sql = "update add_products set p_name='$p_name',category='$category',price ='$price',image='$p_img',desp = '$p_desp' where p_id ='$p_id'";
+    $sql = "update add_products set p_name='$p_name',category='$category',price ='$price1',image='$p_img',desp = '$p_desp' where p_id ='$p_id'";
     $result = mysqli_query($con, $sql);
     if ($result == true) {
         header("location:my_product.php");
